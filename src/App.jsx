7000b3f3c2d8293ee22641dd9c4262b245fba41c
woohyun <woohyun6549@inha.edu>
2023-07-15 13:33:15 +0900

    wrapper 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,12 @@ import "./App.css";
 import styled from "styled-components";
 import Layout from "./sections/Layout";
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #d2daff;
-`;
+
 
 function App() {
   return (
     <>
-
         <Layout />
-        {/* // <h1>Hello, Vite + React + Styled Components!</h1> */}
-
     </>
   );
 }
