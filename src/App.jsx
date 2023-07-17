494fc6c374ca9362dea0c676c34cace1999ddeff
ssukgoon <ssukgoon@gmail.com>
2023-07-17 14:24:56 +0900

    Code: app.jsx 레이아웃 감싸는 wrapper 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,7 @@ const Wrapper = styled.div`
 function App() {
   return (
     <>
-      <Wrapper>
-        <Layout />
-        {/* // <h1>Hello, Vite + React + Styled Components!</h1> */}
-      </Wrapper>
+      <Layout />
     </>
   );
 }
--- src/pages/ai/Ai.jsx
+++ src/pages/ai/Ai.jsx
@@ -1,4 +1,4 @@
 const Ai = () => {
-  return <div>여기는 인공지능페이지입니다.</div>;
+  return <p>여기는 인공지능페이지입니다.</p>;
 };
 export default Ai;
