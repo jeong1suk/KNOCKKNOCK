68f27b61dc2b72274c55dae9e62f09d563f43111
ssukgoon <ssukgoon@gmail.com>
2023-07-19 15:35:34 +0900

    Docs: mainpage 폴더 소문자로 수정

---
+++ src/pages/tmpmainpage/MainPage.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+function MainPage() {
+  return (
+    <>
+      <MainContainer>메인페이지</MainContainer>
+    </>
+  );
+}
+const MainContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  position: relative;
+`;
+
+export default MainPage;
