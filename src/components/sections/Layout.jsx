5e26520df4469bdf564b33c27914a89682f766ed
ssukgoon <ssukgoon@gmail.com>
2023-07-23 23:43:13 +0900

    Feat: 회원가입페이지에서 푸터 안보이게 설정

--- src/components/sections/Layout.jsx
+++ src/components/sections/Layout.jsx
@@ -1,14 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
-import Header from "./Header";
 import Router from "../../routes/Router";
-import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Router />
-      <Footer />
     </BrowserRouter>
   );
 };
