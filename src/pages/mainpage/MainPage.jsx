e2648397cfeac4a44478a8924680429dd757522e
ssukgoon <ssukgoon@gmail.com>
2023-07-24 14:58:25 +0900

    Design: 메인페이지 디자인

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
+import PlayCarousel from "./PlayCarousel";
 function MainPage() {
-
   return (
-    <>
-      <MainContainer>
-        <MainContent01>메인페이지</MainContent01>
-      </MainContainer>
-    </>
+    <MainContainer>
+      <Container>
+        <PlayCarousel />
+      </Container>
+    </MainContainer>
   );
 }
 const MainContainer = styled.div`
@@ -14,10 +14,20 @@ const MainContainer = styled.div`
   margin: 0 auto;
   position: relative;
   margin-top: 200px;
+  @media (max-width: 768px) {
+    /* 화면 너비가 768px 이하일 때의 스타일 */
+    max-width: 90%;
+  }
 `;
 const MainContent01 = styled.div`
   display: flex;
   justify-content: flex-end;
+`;
+const Container = styled.div`
+  width: 100%;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 15px;
 `;
 
 export default MainPage;
