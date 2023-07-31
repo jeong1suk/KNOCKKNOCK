cf7397d17efea150a11c866160d70e61b7ecc65a
ssukgoon <ssukgoon@gmail.com>
2023-07-31 16:58:50 +0900

    CSS: 푸터디자인

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,22 +1,26 @@
 import styled from "styled-components";
 
 const FooterWrapper = styled.footer`
-  background-color: #000;
+  background-color: #fff;
   padding-top: 10px;
   color: white;
   font-size: 0.8em;
   text-align: center;
-  position: absolute;
+
   bottom: 0;
   left: 0;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid black;
 `;
 
 const FooterContent = styled.div`
   display: flex;
   justify-content: space-between;
+
   align-items: center;
-  color: white;
+  color: #000;
   font-size: 0.8rem;
 
   & > div {
@@ -35,9 +39,11 @@ const FooterContent = styled.div`
 const FooterText = styled.p`
   font-style: italic;
   text-align: center;
-  color: white;
+  color: #000;
   font-size: 0.8rem;
   padding-top: 30px;
+  margin-left: 15px;
+  margin-right: 15px;
 `;
 
 const FooterLink = styled.a`
@@ -45,12 +51,21 @@ const FooterLink = styled.a`
   color: white;
   text-decoration: none;
 `;
-
 const Footer = () => {
   return (
     <FooterWrapper>
+      <img
+        src={"006.png"}
+        style={{
+          width: "100px",
+          marginLeft: "50px",
+          maxWidth: "50%",
+          maxHeight: "50%",
+        }}
+      />
+
       <FooterContent>
-        <div>
+        <FooterText>
           낙낙
           <br />
           대표이사 억만추
@@ -60,20 +75,71 @@ const Footer = () => {
           통신판매업신고번호 9999-999-999999
           <br />
           엘리스랩
-        </div>
-        <div>
+        </FooterText>
+        <FooterText>
           이메일 상담 knockknock@ukman.chu
           <br />
           유선 상담 9999-9999
           <br />
           <FooterLink>© UkManChu Co., Ltd.</FooterLink>
-        </div>
+        </FooterText>
+        <FooterText>
+          낙낙 컨텐츠
+          <br />
+          인공지능
+          <br />
+          오늘의 낙낙
+          <br />
+          같이 놀자
+        </FooterText>
+        <FooterText>
+          이은석
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+        <FooterText>
+          정유진
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+        <FooterText>
+          정원석
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+        <FooterText>
+          정재훈
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+        <FooterText>
+          최우현
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+        <FooterText>
+          허창원
+          <br />
+          이메일
+          <br />
+          깃허브
+        </FooterText>
+
+        <FooterText>
+          상품, 상품정보, 거래에 관한 의무와 책임은 판매자에게 있으므로, <br />{" "}
+          각 상품 페이지에서 구체적인 내용을 확인하시기 바랍니다.
+        </FooterText>
       </FooterContent>
-      <FooterText>
-        억만추는 통신판매중개자이며 통신판매 당사자가 아닙니다. 상품, 상품정보,
-        거래에 관한 의무와 책임은 판매자에게 있으므로, 각 상품 페이지에서
-        구체적인 내용을 확인하시기 바랍니다.
-      </FooterText>
     </FooterWrapper>
   );
 };
