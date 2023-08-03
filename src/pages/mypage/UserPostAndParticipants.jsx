cd1b269103f79b7ccb71d94c13b38ae3d138c9be
ssukgoon <ssukgoon@gmail.com>
2023-08-04 01:54:18 +0900

    Chore: 마이페이지 유저 게시글, 참가신청 api 연결

--- src/pages/mypage/UserPostAndParticipants.jsx
+++ src/pages/mypage/UserPostAndParticipants.jsx
@@ -1,9 +1,39 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import * as Api from "../../api";
+import { Link } from "react-router-dom";
 const UserPostAndParticipants = () => {
+  const [posts, setPosts] = useState([]);
+  const [join, setJoin] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await Api.get("/users/mypage/posts");
+      console.log(res.data.posts);
+      setPosts(res.data.posts);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <Userbox>
-      <Postbox>내가 쓴 Post</Postbox>
+      <Postbox>
+        <div>내가 쓴 Post</div>
+        {posts.map((post) => (
+          <div style={{ border: "1px solid #000" }}>
+            <Link to={`/playdetail/${post.postId}`}>
+              <div>내용:{post.content}</div>
+              <div>장소:{post.place}</div>
+              <div>제목:{post.title}</div>
+              <div>type:{post.type}</div>
+            </Link>
+          </div>
+        ))}
+      </Postbox>
+
       <Postbox>참가한 Participants</Postbox>
     </Userbox>
   );
