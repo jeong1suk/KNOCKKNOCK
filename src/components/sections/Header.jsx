6d78742a9947c687d1e84dc41af40693481cf3d3
ssukgoon <ssukgoon@gmail.com>
2023-08-07 21:13:10 +0900

    CSS: 반응형 breakpoint 설정

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -63,13 +63,15 @@ const Header = () => {
 };
 
 const HeaderWrap = styled.div`
+  max-width: 1024px;
   width: 100%;
   height: 3rem;
   background-color: #f7f7f7;
   position: fixed;
   top: 0;
-  left: 0;
+  left: 50%;
   z-index: 998;
+  transform: translate(-50%, 0);
 `;
 
 const HeaderContainer = styled.div`
--- src/components/sections/Layout.jsx
+++ src/components/sections/Layout.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "../../routes/Router";
+import { BaseLayout } from "../layout/Layout";
 import Header from "./Header";
 // import Footer from "./Footer";
 const Layout = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Router />
-      {/* <Footer /> */}
+      <BaseLayout>
+        <Header />
+        <Router />
+        {/* <Footer /> */}
+      </BaseLayout>
     </BrowserRouter>
   );
 };
