82d9a561168fe8fcf0ed5ce2aa263ec37e712f64
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-08-11 03:50:18 +0900

    Fix: 푸터 깃허브 추가 및 링크 추가

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,13 +1,13 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserStateContext } from "../../context/user/UserProvider";
 import styled from "styled-components";
 import { MOBILE_BREAK_POINT } from "../layout/breakpoint";
-import { Link } from "react-router-dom";
 
 const FooterWrapper = styled.footer`
   background-color: #fff;
-  color: white;
+  color: #000; // 글자색을 검은색으로 변경
   font-size: 0.8em;
-  text-align: center;
-
   bottom: 0;
   left: 0;
   width: 100%;
@@ -17,106 +17,98 @@ const FooterWrapper = styled.footer`
   justify-content: space-between;
   border-top: 1px solid black;
   @media (max-width: ${MOBILE_BREAK_POINT}) {
-    /* 화면 너비가 768px 이하일 때 폰트 크기 조정 */
-
+    /* 화면 너비가 768px 이하일 때 footer 안보임 */
     display: none;
   }
 `;
 
-const FooterContent = styled.div`
-  display: flex;
-  // justify-content: space-between;
-
-  align-items: center;
-  color: #000;
-  font-size: 0.8rem;
-
-  & > div {
-    flex: 1;
-  }
+const LogoImage = styled.img`
+  width: 15%;
+  height: auto;
+  margin-top: 10px;
+  margin-left: 40px;
 `;
-// const FooterContent = styled.div`
-//   display: flex;
-//   flex-direction: column; /* 변경: 내용을 세로로 정렬 */
-//   align-items: center; /* 변경: 가운데로 정렬 */
-//   color: white;
-//   font-size: 0.8rem;
-//   margin-bottom: 20px; /* 추가: 내용과 하단 간격 설정 */
-// `;
 
 const MemberContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 200px;
+  width: 210px;
+  text-align: left;
+  margin: 10px;
 `;
 
-const Member = styled.div`
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 180px;
+  text-align: left;
+  margin: 10px;
+`;
+
+const FrontMember = styled.div`
+  padding-left: 1rem;
+  margin-top: 5px;
+  white-space: nowrap; // 텍스트를 한 줄에 표시
+  overflow: hidden; // 텍스트가 너비를 초과하면 잘림
+  text-overflow: ellipsis; // 텍스트가 너비를 초과하면 ... 으로 표시
+  max-width: 100%; // 컨테이너의 최대 너비를 지정
+`;
+
+const BackMember = styled.div`
+  padding-left: 2rem;
   margin-top: 5px;
 `;
 
 const FooterText = styled.div`
   display: flex;
-  flex-direction: column; /*변경: 내용을 세로로 정렬*/
-  // font-style: italic;
+  flex-direction: column;
   font-weight: 600;
   text-align: center;
   color: #000;
   font-size: 0.8rem;
-  /* padding-top: 1rem; */
-  // margin-left: 1rem;
-  // margin-right: 3rem;
+  margin: 0; // 기존 마진 제거
 `;
 
-const FooterLink = styled.a`
-  font-weight: 900;
-  color: white;
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  color: #000; // 원하는 색상
   text-decoration: none;
+  margin: 0.2rem;
+  font-weight: 600;
+  &:hover {
+    text-decoration: underline; // 마우스를 올렸을 때 밑줄 추가
+  }
 `;
+
 const Footer = () => {
+  const { user } = useContext(UserStateContext);
+
   return (
     <FooterWrapper>
-      <img
-        src={"/006.png"}
-        style={{
-          width: "10%",
-          height: "70%",
-          marginTop: "30px",
-          marginLeft: "40px",
-          // maxWidth: "50%",
-          // maxHeight: "50%",
-        }}
-      />
-      <FooterContent>
-        <FooterText>
-          KnockKnock
-          <br />
-          <br />
-          <Link to="/ai">뷰티톡톡</Link>
-          <br />
-          <Link to="/todayknock">오늘의 낙낙</Link>
-          <br />
-          <Link to="/play">히히낙낙</Link>
-          <br />
-          마이페이지
-        </FooterText>
-        <MemberContainer>
-          <FooterText>
-            Member
-            <br />
-            <br />
-          </FooterText>
-          <Member>이은석 github @enxxi</Member>
-          <Member>정유진 github @nanyoojinee</Member>
-          <Member>정원석 github @jeong1suk</Member>
-        </MemberContainer>
-        <MemberContainer>
-          <br />
-          <br />
-          <Member>정재훈 github @J-A-Y2</Member>
-          <Member>최우현 github @choiwoohyun123</Member>
-          <Member>허창원 github @wonn23</Member>
-        </MemberContainer>
-      </FooterContent>
+      <LogoImage src={"/006.png"} alt="Logo" />
+      <MemberContainer>
+        <FooterText>Front</FooterText>
+        <FrontMember>정원석 github @jeong1suk</FrontMember>
+        <FrontMember>최우현 github @choiwoohyun123</FrontMember>
+        <FrontMember>정유진 github @nanyoojinee</FrontMember>
+      </MemberContainer>
+      <MemberContainer>
+        <FooterText>Back</FooterText>
+        <BackMember>허창원 github @wonn23</BackMember>
+        <BackMember>정재훈 github @J-A-Y2</BackMember>
+        <BackMember>이은석 github @enxxi</BackMember>
+      </MemberContainer>
+      <LinkContainer>
+        <FooterText>KnockKnock</FooterText>
+        <StyledLink to="/ai">뷰티톡톡</StyledLink>
+        {user && (
+          <>
+            <StyledLink to="/todayknock">오늘의 낙낙</StyledLink>
+            <StyledLink to="/play">히히낙낙</StyledLink>
+          </>
+        )}
+      </LinkContainer>
     </FooterWrapper>
   );
 };
