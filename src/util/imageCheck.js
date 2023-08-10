7b35c9ce555fa08141f6397f87d2085896ea7691
woohyun <woohyun6549@inha.edu>
2023-08-10 20:31:14 +0900

    기본이미지 변경

--- src/util/imageCheck.js
+++ src/util/imageCheck.js
@@ -2,6 +2,6 @@ export const getImageSrc = (imageUrl) => {
   if (imageUrl) {
     return imageUrl;
   } else {
-    return "http://placekitten.com/200/200";
+    return "/defaultImage.png"; // 웹 서버의 루트를 기반으로 합니다.
   }
 };
